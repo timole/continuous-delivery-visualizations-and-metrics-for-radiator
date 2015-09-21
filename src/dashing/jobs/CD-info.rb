fa18d62948b3d27701ac05ecb000d370594e6f86
Timo Lehtonen <timo.lehtonen@gmail.com>
2015-09-21 13:36:50 +0300

    Commenting

--- src/dashing/jobs/CD-info.rb
+++ src/dashing/jobs/CD-info.rb
@@ -1,5 +1,6 @@
 VCS_ROOT = RADIATOR_CONFIG[:vcs_root]
 SCHEDULER.every '15m', first_in: 0 do
+  # VCS_ROOT variable is read from a config file
   output = `jobs/CD-info-get.sh '#{VCS_ROOT}'`
   val = output.match(/.* (.*)/)[1]
 
